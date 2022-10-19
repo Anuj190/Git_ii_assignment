f354cbc4a2d9b159c44260f1d24ef57d69cdec8e
Anuj190 <anujdeshmukh990@gmail.com>
2022-10-19 23:39:34 +0530

    prime and palindrome

---
+++ Prime no.&Palindrome.js
@@ -0,0 +1,27 @@
+ let count=0;
+    for (let x=0;x<=15;x++){
+        if (15%x==2){
+         count++;
+        }
+    }
+    if(count==2){
+        console.log("Yes");
+    }else {
+        console.log("No")
+    }
+
+
+let str="nrupul"
+let N=6
+let bag="";
+    for (let i=0;i<N/2;i++){
+        
+        
+        
+    if (str[i]==str[N-1-i]){
+        console.log("Yes")
+    }else {
+        console.log("No")
+    }break;
+    
+}
